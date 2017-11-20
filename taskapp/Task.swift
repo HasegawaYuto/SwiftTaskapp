98caa17ece6cf294785da8ab3af5be3b7318efa3
長谷川勇斗 <hasegawayuto@hasegawaisamutonoMac-mini.local>
2017-11-20 14:25:13 +0900

    notifications

---
+++ taskapp/Task.swift
@@ -0,0 +1,30 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 長谷川勇斗 on 2017/11/19.
+//  Copyright © 2017年 長谷川勇斗. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
